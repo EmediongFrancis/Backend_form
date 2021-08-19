e17b63a3df90244e6c2e4c5ae17eacee2db1f867
Emediong Francis <emediongfrancis@gmail.com>
2021-08-19 12:21:03 +0100

    added and configured to accept phpmailer

--- form_mail.php
+++ form_mail.php
@@ -1,8 +1,15 @@
 <?php 
 
-require 'PHPMailer-master/PHPMailerAutoload.php';
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
 
-$mail = new PHPMailer;
+//Load Composer's autoloader
+require './vendor/autoload.php';
+
+$mail = new PHPMailer();
 
     $mail->IsSMTP();  // telling the class to use SMTP
     $mail->SMTPAuth   = true; // SMTP authentication
