a3ab124b71abc5067d94aa8faea85296dc26d84e
Emediong Francis <emediongfrancis@gmail.com>
2021-08-19 11:46:54 +0100

    added and configured to accept phpmailer

--- form_mail.php
+++ form_mail.php
@@ -12,36 +12,36 @@ require 'vendor/autoload.php';
 //Create an instance; passing `true` enables exceptions
 $mail = new PHPMailer(true);
 
-//Server settings
-$mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
-$mail->isSMTP();                                            //Send using SMTP
-$mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
-$mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-$mail->Username   = 'emediongfrancis@gmail.com';                     //SMTP username
-$mail->Password   = 'tapadawaldma123987';                               //SMTP password
-// $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-$mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+try {
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'emediongfrancis@gmail.com';                     //SMTP username
+    $mail->Password   = 'tapadawaldma123987';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
 
-$name = $_POST['name'];
-$email = $_POST['email'];
-$message = $_POST['message'];
-$content = "Name: " + $name + " Message: " + $message;
-$to = "emediongfrancis@gmail.com";
-$subject = "Contact Form";
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    $content = "Name: " + $name + " Message: " + $message;
+    $to = "emediongfrancis@gmail.com";
+    $subject = "Contact Form";
 
-//Recipients
-$mail->setFrom($email, $name);
-$mail->addAddress($to, 'Emediong Francis');
+    //Recipients
+    $mail->setFrom($email, $name);
+    $mail->addAddress($to, 'Emediong Francis');
 
-//Content
-// $mail->isHTML(true);                                  //Set email format to HTML
-$mail->Subject = $subject;
-$mail->Body    = $content;
+    //Content
+    // $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = $subject;
+    $mail->Body    = $content;
 
-if(!$mail->Send()) {
-    echo 'Message was not sent.';
-    echo 'Mailer error: ' . $mail->ErrorInfo;
-  } else {
-    echo 'Message has been sent.';
-  }
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
