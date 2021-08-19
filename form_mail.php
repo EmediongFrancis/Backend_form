66c9ef270692a250288772140e818f2508647ee0
Emediong Francis <emediongfrancis@gmail.com>
2021-08-20 00:42:11 +0100

    smtp.

--- form_mail.php
+++ form_mail.php
@@ -18,18 +18,18 @@ $mail = new PHPMailer;
     $mail->SMTPSecure = 'tls';
     $mail->Port       = 587; // SMTP Port
     $mail->Username   = "emediongfrancis@gmail.com"; // SMTP account username
-    $mail->Password   = "tapadawaldma123987";        // SMTP account password
+    $mail->Password   = "wytzpavfcmfzefyb";        // SMTP account password
 
-    $mail->SetFrom('kennyekandem@gmail.com', 'Kenneth Ekandem'); // FROM
+    $mail->SetFrom('emediongfrancis@gmail.com', 'Emediong Francis'); // FROM
 
-    $mail->AddAddress('emediongfrancis@gmail.com', 'emediong francis'); // recipient email
+    $mail->AddAddress('emediongfrancis@gmail.com', 'Emediong Francis'); // recipient email
 
     $mail->Subject    = "First SMTP Message"; // email subject
     $mail->Body       = "Hi! \n\n This is my first e-mail sent through Google SMTP using PHPMailer.";
 
     if(!$mail->Send()) {
-      echo 'Message was not sent.';
+      echo 'Message was not sent, please try again.';
       echo 'Mailer error: ' . $mail->ErrorInfo;
     } else {
-      echo 'Message has been sent.';
+      echo 'Message has been sent successfully.';
     }
